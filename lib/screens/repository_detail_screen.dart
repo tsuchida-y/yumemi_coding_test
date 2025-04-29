ea2935892ddc25d728b53d9edd7073ce9970e3e6
tsuchida <yu41926t.tutida@icloud.com>
2025-04-29 21:58:56 +0900

    style:ラベルと値を揃えて表示するメソッドを追加

--- lib/screens/repository_detail_screen.dart
+++ lib/screens/repository_detail_screen.dart
@@ -15,21 +15,44 @@ class RepositoryDetailScreen extends StatelessWidget {
       appBar: AppBar(title: Text(repository.name)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(repository.ownerAvatarUrl),
-            ),
-            SizedBox(height: 20),
-            Text("Language: ${repository.language}"),
-            Text("Stars: ${repository.stars}"),
-            Text("Watchers: ${repository.watchers}"),
-            Text("Forks: ${repository.forks}"),
-            Text("Open Issues: ${repository.openIssues}"),
-          ],
+        child: Center(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(repository.ownerAvatarUrl),
+              ),
+              SizedBox(height: 10),
+              _buildDetailRow("言語", repository.language),
+              SizedBox(height: 10),
+              _buildDetailRow("スター数", repository.stars.toString()),
+              SizedBox(height: 10),
+              _buildDetailRow("ウォッチャー数", repository.watchers.toString()),
+              SizedBox(height: 10),
+              _buildDetailRow("フォーク数", repository.forks.toString()),
+              SizedBox(height: 10),
+              _buildDetailRow("未解決の課題", repository.openIssues.toString()),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  /// ラベルと値を揃えて表示する行を構築するヘルパーメソッド
+  Widget _buildDetailRow(String label, String value) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          value,
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
     );
   }
 }

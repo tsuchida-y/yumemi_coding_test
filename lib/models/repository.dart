c423e8f9d8cf498f950b9978204a8bf8e09cb85d
tsuchida <yu41926t.tutida@icloud.com>
2025-05-04 01:56:13 +0900

    perf:変数の縦を合わせて、可読性を向上させた

--- lib/models/repository.dart
+++ lib/models/repository.dart
@@ -1,6 +1,5 @@
 
-//GitHub API のレスポンスから取得したリポジトリ情報を表現するためのモデルクラス
-
+///GitHub API のレスポンスから取得したリポジトリ情報を表現するためのモデルクラス
 class Repository {
   final String name;
   final String ownerAvatarUrl;
@@ -23,13 +22,13 @@ class Repository {
   /// JSONデータをRepositoryオブジェクトに変換するファクトリメソッド。
   factory Repository.fromJson(Map<String, dynamic> repositoryJson) {
     return Repository(
-      name: repositoryJson['name'],
+      name:           repositoryJson['name'],
       ownerAvatarUrl: repositoryJson['owner']['avatar_url'],
-      language: repositoryJson['language'] ?? 'N/A',
-      stars: repositoryJson['stargazers_count'],
-      watchers: repositoryJson['watchers_count'],
-      forks: repositoryJson['forks_count'],
-      openIssues: repositoryJson['open_issues_count'],
+      language:       repositoryJson['language'] ?? 'N/A',
+      stars:          repositoryJson['stargazers_count'],
+      watchers:       repositoryJson['watchers_count'],
+      forks:          repositoryJson['forks_count'],
+      openIssues:     repositoryJson['open_issues_count'],
     );
   }
 }
